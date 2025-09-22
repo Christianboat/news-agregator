2e22baea541f6bc51540a727934658d57405d348
Christian Boateng <Christianboateng360@gmail.com>
2025-09-22 21:52:26 +0000

    Update config.py
    Removed all APIs and secret keys

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'c4a2f8b9e7d3f1a5c6e9b2a8d7f3c1e4a9f6b2d7e3c8a1f5d9b4e7c2f6a8d3c9'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Your_Secret_key_here'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
    
     # Telegram configuration
-    TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN') or '7919647588:AAE21PYxU4bC08PmvfC_IFRU1p_FgpOkwGo'
-    TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID') or '5425768848'
+    TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN') or 'Your_telegram_bot_token_here'
+    TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID') or 'Your_telegram_chat_id_here'
    
     # Gemini API
-    GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY') or 'AIzaSyDpGTJ9FsbKdogPsUAOHdkZxDDRbrXdxfk'
+    GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY') or 'Your_gemini_api_key_here'
    
     # Image storage
     UPLOAD_FOLDER = os.path.join(basedir, 'app/static/images')
@@ -24,19 +24,11 @@ class Config:
    
     # RSS feeds
     RSS_FEEDS = [
-        "https://www.myjoyonline.com/feed",
-        "https://www.pulse.com.gh/rss",
-        "https://www.ghanaweb.com/feed/",
-        "https://citinewsroom.com/feed",
-        "https://www.modernghana.com/rssfeed/news.xml",
-        "https://www.adomonline.com/feed",
-        "https://www.ghanaiantimes.com.gh/feed",
-        "https://www.peacefmonline.com/pages/rss/",
-        "https://www.ghheadlines.com/rss",
-        "https://allafrica.com/tools/headlines/rdf/ghana/headlines.rdf"
+        #list all rss_feeds of site you want scrape here
     ]
    
     EDUCATION_KEYWORDS = [
+        #you can chnage these key words to key words of the content type you want eg politices, health, etc
         "education", "school", "student", "teacher", "university", "college",
         "classroom", "curriculum", "learning", "training", "scholarship",
         "academic", "exam", "lecturer", "institution"
